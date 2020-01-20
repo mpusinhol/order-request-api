027a2b4f0458b9869141b465f51c0860f4698045
Matheus Pusinhol <matheus.pusinhol.ext@dasa.com.br>
2020-01-20 15:09:12 -0300

    Add categories mock in h2 database

--- src/main/java/com/mpusinhol/orderquest/OrderRequestApiApplication.java
+++ src/main/java/com/mpusinhol/orderquest/OrderRequestApiApplication.java
@@ -1,13 +1,31 @@
 package com.mpusinhol.orderquest;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mpusinhol.orderquest.domain.Category;
+import com.mpusinhol.orderquest.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class OrderRequestApiApplication {
+public class OrderRequestApiApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderRequestApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category category1 = new Category(null, "IT");
+		Category category2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(category1, category2));
 	}
 
 }
